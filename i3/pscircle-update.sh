574125c0a9538be38c8bda84a2db5419e1f9f2a6
Predaking2612 <33420975+Predaking2612@users.noreply.github.com>
2019-06-07 13:41:02 -0400

    Add files via upload

---
+++ i3/pscircle-update.sh
@@ -0,0 +1,6 @@
+while true;
+do 
+	pscircle \
+	--output=/home/predaking2612/Pictures/pscircle.png
+	sleep 2
+done
---
+++ i3/refresh.sh
@@ -0,0 +1,5 @@
+while true
+do
+	feh --bg-fill ~/Pictures/pscircle.png
+	sleep 4
+done
