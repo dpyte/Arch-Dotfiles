26a58e819f2d1b03eb726aa98fd9a82d1862e9d9
Predaking2612 <pateldrv456@gmail.com>
2019-08-26 19:09:20 -0400

    @up

--- polybar/OLD/launch.sh
+++ polybar/OLD/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars
-polybar top &
+# Launch bar1 and bar2
 polybar bottom &
-# polybar example &
+polybar top &
+
+echo "Bars launched..."
