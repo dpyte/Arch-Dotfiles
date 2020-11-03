a965f16d32786fb50e0e7d2306912b28139efa4f
Predaking2612 <predaking2612@gmail.com>
2020-11-02 19:32:32 -0500

    update config for fish

---
+++ fish/config.fish
@@ -0,0 +1,229 @@
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 3
+
+abbr -a e nvim
+
+# colored man output
+# from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+setenv LESS_TERMCAP_me \e'[0m'           # end mode
+setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m'           # end underline
+setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
+setenv FZF_CTRL_T_COMMAND 'fd --type file --follow'
+setenv FZF_DEFAULT_OPTS '--height 20%'
+
+function fish_user_key_bindings
+	bind \cz 'fg>/dev/null ^/dev/null'
+	if functions -q fzf_key_bindings
+		fzf_key_bindings
+	end
+end
+
+# Auto jump
+set -gx AUTOJUMP_SOURCED 1
+
+# set user installation path
+if test -d ~/.autojump
+    set -x PATH ~/.autojump/bin $PATH
+end
+
+# Set ostype, if not set
+if not set -q OSTYPE
+    set -gx OSTYPE (bash -c 'echo ${OSTYPE}')
+end
+
+
+# enable tab completion
+complete -x -c j -a '(autojump --complete (commandline -t))'
+
+
+# set error file location
+if test (uname) = "Darwin"
+    set -gx AUTOJUMP_ERROR_PATH ~/Library/autojump/errors.log
+else if test -d "$XDG_DATA_HOME"
+    set -gx AUTOJUMP_ERROR_PATH $XDG_DATA_HOME/autojump/errors.log
+else
+    set -gx AUTOJUMP_ERROR_PATH ~/.local/share/autojump/errors.log
+end
+
+if test ! -d (dirname $AUTOJUMP_ERROR_PATH)
+    mkdir -p (dirname $AUTOJUMP_ERROR_PATH)
+end
+
+
+# change pwd hook
+function __aj_add --on-variable PWD
+    status --is-command-substitution; and return
+    autojump --add (pwd) >/dev/null 2>>$AUTOJUMP_ERROR_PATH &
+end
+
+
+# misc helper functions
+function __aj_err
+    # TODO(ting|#247): set error file location
+    echo -e $argv 1>&2; false
+end
+
+# default autojump command
+function j
+    switch "$argv"
+        case '-*' '--*'
+            autojump $argv
+        case '*'
+            set -l output (autojump $argv)
+            # Check for . and attempt a regular cd
+            if [ $output = "." ]
+                cd $argv
+            else
+                if test -d "$output"
+                    set_color red
+                    echo $output
+                    set_color normal
+                    cd $output
+                else
+                    __aj_err "autojump: directory '"$argv"' not found"
+                    __aj_err "\n$output\n"
+                    __aj_err "Try `autojump --help` for more information."
+                end
+            end
+    end
+end
+
+
+# jump to child directory (subdirectory of current path)
+function jc
+    switch "$argv"
+        case '-*'
+            j $argv
+        case '*'
+            j (pwd) $argv
+    end
+end
+
+
+# open autojump results in file browser
+function jo
+    set -l output (autojump $argv)
+    if test -d "$output"
+        switch $OSTYPE
+            case 'linux*'
+                xdg-open (autojump $argv)
+            case 'darwin*'
+                open (autojump $argv)
+            case cygwin
+                cygstart "" (cygpath -w -a (pwd))
+            case '*'
+                __aj_err "Unknown operating system: \"$OSTYPE\""
+        end
+    else
+        __aj_err "autojump: directory '"$argv"' not found"
+        __aj_err "\n$output\n"
+        __aj_err "Try `autojump --help` for more information."
+    end
+end
+
+
+# open autojump results (child directory) in file browser
+function jco
+    switch "$argv"
+        case '-*'
+            j $argv
+        case '*'
+            jo (pwd) $argv
+    end
+end
+
+function d
+	while test $PWD != "/"
+		if test -d .git
+			break
+		end
+		cd ..
+	end
+end
+
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n 'λ '
+	set_color normal
+end
+
+function fish_greeting
+	echo
+	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e " \\e[1mDisk usage:\\e[0m"
+	echo
+	echo -ne (\
+		df -l -h | grep -E 'dev/(xvda|sd|mapper)' | \
+		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
+		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
+		paste -sd ''\
+	)
+	echo
+
+	echo -e " \\e[1mNetwork:\\e[0m"
+	echo
+	# http://tdt.rocks/linux_network_interface_naming.html
+	echo -ne (\
+		ip addr show up scope global | \
+			grep -E ': <|inet' | \
+			sed \
+				-e 's/^[[:digit:]]\+: //' \
+				-e 's/: <.*//' \
+				-e 's/.*inet[[:digit:]]* //' \
+				-e 's/\/.*//'| \
+			awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
+			sort | \
+			column -t -R1 | \
+			# public addresses are underlined for visibility \
+			sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
+			# private addresses are not \
+			sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
+			# unknown interfaces are cyan \
+			sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
+			# ethernet interfaces are normal \
+			sed 's/\(\(en\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
+			# wireless interfaces are purple \
+			sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
+			# wwan interfaces are yellow \
+			sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
+			sed 's/$/\\\e[0m/' | \
+			sed 's/^/\t/' \
+		)
+	echo
+end
+
+export XAUTHORITY=$HOME/.Xauthority
+export TERM=xterm-256color
+
+alias gd="git diff"
+alias gs="git status"
+alias ..="cd .."
+alias vim "nvim"
+alias ls "exa"
+alias ll "exa -l"
+alias l "exa -lh"
