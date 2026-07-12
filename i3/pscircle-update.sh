a140776732a6a80e513202406a0be3f4ab12ad73
Dhruv Patel <33420975+dpyte@users.noreply.github.com>
2026-07-12 00:51:16 -0400

    Update packages/deps. Organized stuff and remove old garbage

--- i3/pscircle-update.sh
+++
@@ -1,6 +0,0 @@
-while true;
-do 
-	pscircle \
-	--output=/home/predaking2612/Pictures/pscircle.png
-	sleep 2
-done
--- i3/refresh.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-	feh --bg-fill ~/Pictures/pscircle.png
-	sleep 4
-done
